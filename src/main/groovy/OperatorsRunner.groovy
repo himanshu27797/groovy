a60494c1ad142191c672008f2dc3c7e7bc055c18
kirill.medvedev <k30medvedev@gmail.com>
2022-01-28 21:09:21 +0300

    assert

---
+++ src/main/groovy/OperatorsRunner.groovy
@@ -0,0 +1,18 @@
+assert 1 + 2 == 3
+assert 4 - 3 == 1
+assert 3 * 5 == 15
+assert 3 / 2 == 1.5
+assert 3.intdiv(2) == 1
+assert 10 % 3 == 1
+assert 2**2 == 4
+
+def result = 3 / 2 //1.5
+def result2 = 3 / 2 as Integer //1 def as Type(clazz)
+
+
+Integer integer1 = 1
+Integer integer2 = 1
+
+assert integer1 == integer2 //true
+assert integer1 === integer2 //true
+
