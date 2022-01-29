9e3d6e43d52fd8d1e8e05f7a71e921961ee7eff2
kirill.medvedev <k30medvedev@gmail.com>
2022-01-29 18:53:38 +0300

    map

---
+++ src/main/groovy/MapRunner.groovy
@@ -0,0 +1,25 @@
+class MapRunner {
+    static void main(String[] args) {
+        def emptyMap = [:]
+        def map = ["one": 1, "two": 2]
+        assert map.getClass() == LinkedHashMap
+
+        //read
+        assert map.get("one") == 1
+        assert map["one"] == 1
+        assert map.one == 1
+
+        //write
+        //        map.one = 111
+        map.three = 3
+
+        //new method
+        assert map.subMap("one", "two") == [one: 1, two: 2]
+
+
+        //new operator
+        def newMap = ["q": 22, "rt": 99, *:map]
+        println newMap
+
+    }
+}
